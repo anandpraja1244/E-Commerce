d97d6491a0c58b749a972b0f115b77842af78851
anandpraja1244 <anandpraja1244@gmail.com>
2021-08-31 10:06:53 +0530

    first commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Banner = () => {
+    return(
+        <div className="container">
+        <header style={{marginTop: '70px' ,color:'blue'}}>
+         <div className="headerText">
+         <div>
+         <h1>Holi dhamaka Enjoy</h1>
+         <p>Enjoy online shopping</p>
+         </div>
+         </div>
+        </header>
+        </div>
+    )
+}
+
+export default Banner;
